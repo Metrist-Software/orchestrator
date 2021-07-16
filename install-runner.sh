1abefa6c5941f7ec401b962bdeaa44797acd94e3
Cees de Groot <cg@evrl.com>
2021-07-16 15:20:06 -0400

    Adjust spelling of runner install message a bit

--- install-runner.sh
+++ install-runner.sh
@@ -6,7 +6,7 @@
 set -euo pipefail
 
 latest=$(curl https://canary-public-assets.s3.us-west-2.amazonaws.com/dist/monitors/latest.txt)
-echo "Installing runner from Git tag $latest"
+echo "Installing runner version $latest"
 curl https://canary-public-assets.s3.us-west-2.amazonaws.com/dist/monitors/runner-${latest}-linux-x64.zip >/tmp/runner.zip
 cd priv/runner
 unzip /tmp/runner.zip
