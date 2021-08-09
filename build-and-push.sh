ae557737171055af1aa3af5c37e16d36eb033914
Cees de Groot <cg@evrl.com>
2021-08-09 17:24:58 -0400

    Dogfood public docker image

--- build-and-push.sh
+++ build-and-push.sh
@@ -3,17 +3,17 @@
 #
 #  Build and push the docker container
 #
-ecr_name=147803588724.dkr.ecr.us-west-2.amazonaws.com
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ecr_name
 version=$(git rev-parse --short HEAD)
-image_tag=$ecr_name/orchestrator:$version
+image_tag=canarymonitor/agent:$version
 
+# Note that this'll for now build "orchestrator" which we then
+# tag as agent and push under that name (transitionally).
 make release
 docker tag orchestrator:$version $image_tag
 docker push $image_tag
 
-# This is strictly not needed, but _might_ come in handy when testing.
-latest_tag=$ecr_name/orchestrator:latest
+# This is what the Helm chart uses by default.
+latest_tag=canarymonitor/agent:latest
 docker tag $image_tag $latest_tag
 docker push $latest_tag
 
