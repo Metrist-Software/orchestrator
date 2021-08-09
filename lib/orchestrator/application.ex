b1c7d12d22aa5b5422c68a14857ce10e4bc138db
Cees de Groot <cg@evrl.com>
2021-08-09 16:25:01 -0400

    Support more logging levels

--- lib/orchestrator/application.ex
+++ lib/orchestrator/application.ex
@@ -65,11 +65,18 @@ defmodule Orchestrator.Application do
   defp do_parse_bool("0"), do: false
   defp do_parse_bool(_), do: false # Safe default.
 
+  defp set_logging("all"), do: Logger.configure(level: :debug)
+  defp set_logging("none"), do: Logger.configure(level: :none)
+
   defp set_logging("debug"), do: Logger.configure(level: :debug)
   defp set_logging("info"), do: Logger.configure(level: :info)
-  defp set_logging("information"), do: Logger.configure(level: :info)
+  defp set_logging("information"), do: Logger.configure(level: :info)  # For C# compat
+  defp set_logging("notice"), do: Logger.configure(level: :notice)
   defp set_logging("warning"), do: Logger.configure(level: :warning)
   defp set_logging("error"), do: Logger.configure(level: :error)
+  defp set_logging("critical"), do: Logger.configure(level: :critical)
+  defp set_logging("alert"), do: Logger.configure(level: :alert)
+  defp set_logging("emergency"), do: Logger.configure(level: :emergency)
 
 if Mix.env() == :test do
   # For now, the simplest way to make tests just do tests, not configure/start anything.
