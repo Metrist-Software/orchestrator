edf9fc6ca6587c380ff2edfa27f632cedc589648
Cees de Groot <cg@evrl.com>
2021-08-09 11:38:27 -0400

    Also take the configured steps into account when comparing configs

--- test/orchestrator/configuration_test.exs
+++ test/orchestrator/configuration_test.exs
@@ -75,4 +75,42 @@ defmodule Orchestrator.ConfigurationTest do
     assert length(deltas.delete) == 0
     assert length(deltas.change) == 1
   end
+
+  test "Monitors that have different checks are different monitors" do
+    new = %{
+      monitors: [
+        %{
+          monitor_logical_name: "foodog",
+          steps: [
+            %{check_logical_name: "check_two"}
+          ],
+          interval_secs: 120
+        },
+        %{
+          monitor_logical_name: "foodog",
+          steps: [
+            %{check_logical_name: "check_one"}
+          ],
+          interval_secs: 60
+        }
+      ]
+    }
+
+    old = %{
+      monitors: [
+        %{
+          monitor_logical_name: "foodog",
+          steps: [
+            %{check_logical_name: "check_one"}
+          ],
+          interval_secs: 60
+        }
+      ]
+    }
+
+    deltas = diff_config(new, old)
+    assert length(deltas.add) == 1
+    assert length(deltas.delete) == 0
+    assert length(deltas.change) == 0
+  end
 end
