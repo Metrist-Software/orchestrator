450ad3fe44201c59b5547789892ccfc240aee8a1
Cees de Groot <cg@evrl.com>
2021-08-10 11:03:36 -0400

    Implement preview mode, simplify configuration and align bools with C# runner code

--- lib/orchestrator/application.ex
+++ lib/orchestrator/application.ex
@@ -2,38 +2,21 @@ defmodule Orchestrator.Application do
   use Application
   require Logger
 
+  @known_secrets_managers %{
+    "aws" => Orchestrator.AWSSecretsManager
+  }
+
   @impl true
   def start(_type, _config) do
     print_header()
 
-    region = System.get_env("AWS_REGION", "fake-dev-region")
-    Application.put_env(:orchestrator, :aws_region, region)
-
-    instance = System.get_env("CANARY_INSTANCE_ID", "fake-dev-instance")
-    Application.put_env(:orchestrator, :instance, instance)
-
     rg_string = System.get_env("CANARY_RUN_GROUPS", "")
     run_groups = parse_run_groups(rg_string)
-
-    # TODO set default invocation style back to rundll
-    invocation_style = System.get_env("CANARY_INVOCATION_STYLE", "awslambda")
-    Application.put_env(:orchestrator, :invocation_style, invocation_style)
-
-    cl_string = System.get_env("CANARY_CLEANUP_ENABLED", "false")
-    cleanup_enabled = parse_bool(cl_string)
-    Application.put_env(:orchestrator, :cleanup_enabled, cleanup_enabled)
-
-    ss_string = System.get_env("CANARY_SECRETS_SOURCE")
-    secrets_source = Map.get(%{"aws" => Orchestrator.AWSSecretsManager}, ss_string, Orchestrator.AWSSecretsManager)
-    Application.put_env(:orchestrator, :secrets_source, secrets_source)
-
-    cma_config = System.get_env("CANARY_CMA_CONFIG")
-    Application.put_env(:orchestrator, :cma_config, cma_config)
 
     ll_config = System.get_env("CANARY_LOGGING_LEVEL", "Info")
     set_logging(String.downcase(ll_config))
 
-    config_fetch_fun = fn -> Orchestrator.APIClient.get_config(instance, run_groups) end
+    config_fetch_fun = fn -> Orchestrator.APIClient.get_config(instance(), run_groups) end
 
     configure_api_token()
 
@@ -47,23 +30,30 @@ defmodule Orchestrator.Application do
     Supervisor.start_link(children, opts)
   end
 
-  def instance, do: Application.get_env(:orchestrator, :instance)
-  def secrets_source, do: Application.get_env(:orchestrator, :secrets_source)
-  def do_cleanup?, do: Application.get_env(:orchestrator, :cleanup_enabled)
-  def invocation_style, do: Application.get_env(:orchestrator, :invocation_style)
-  def cma_config, do: Application.get_env(:orchestrator, :cma_config)
+  def instance, do: System.get_env("CANARY_INSTANCE_ID", "fake-dev-instance")
+
+  def do_cleanup?, do: System.get_env("CANARY_CLEANUP_ENABLED") != nil
+
+  # TODO set default invocation style back to rundll
+  def invocation_style, do: System.get_env("CANARY_INVOCATION_STYLE", "aws_lambda")
+
+  def cma_config, do: System.get_env("CANARY_CMA_CONFIG")
+
+  def preview_mode?, do: System.get_env("CANARY_PREVIEW_MODE") != nil
+
+  def secrets_source do
+    env = System.get_env("CANARY_SECRETS_SOURCE")
+    Map.get(@known_secrets_managers, env, Orchestrator.AWSSecretsManager)
+  end
+
+  def aws_region, do: System.get_env("AWS_REGION", "fake-dev-region")
+
+  def api_token, do: Application.get_env(:orchestrator, :api_token)
 
   defp parse_run_groups(""), do: []
   defp parse_run_groups(string) do
     String.split(string, ",")
   end
-
-  defp parse_bool(s), do: do_parse_bool(String.downcase(s))
-  defp do_parse_bool("true"), do: true
-  defp do_parse_bool("false"), do: false
-  defp do_parse_bool("1"), do: true
-  defp do_parse_bool("0"), do: false
-  defp do_parse_bool(_), do: false # Safe default.
 
   defp set_logging("debug"), do: Logger.configure(level: :debug)
   defp set_logging("info"), do: Logger.configure(level: :info)
