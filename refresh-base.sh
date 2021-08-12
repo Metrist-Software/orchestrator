02719a26834c9c3b49fa12ca95fde80ee037418b
Cees de Groot <cg@evrl.com>
2021-08-12 17:22:05 -0400

    Base dockerfiles and refresh script

---
+++ refresh-base.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+#  Refresh base containers. This is supposed to run once a week.
+#
+set -euo pipefail
+
+tag=base-$(date +%Y.%W)
+image=canarymonitor/agent
+
+for i in build runtime
+do
+    full_version=$image:${i}-${tag}
+    docker build -f Dockerfile.${i}-base -t $full_version .
+    docker push $full_version
+done
+
+sed -Ei "s/-base-[0-9]{4}\.[0-9]+/-${tag}/" Dockerfile
+git commit -m "Bump base tag to ${tag}" Dockerfile
+git push
