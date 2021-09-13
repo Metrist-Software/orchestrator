c21a1f8cc4d557e773ccba294a5dc9f07addaef8
Dave Gray <dave@davegray.ca>
2021-09-13 12:25:44 -0400

    Pull preview runner unless in main

--- install-runner.sh
+++ install-runner.sh
@@ -5,9 +5,18 @@
 #
 set -euo pipefail
 
+case "$GITHUB_REF" in
+    refs/heads/main)
+        qualifier=""
+        ;;
+    *)
+        qualifier="-preview"
+        ;;
+esac
+
 dist=https://monitor-distributions.canarymonitor.com
 
-latest=$(curl $dist/runner-latest.txt)
+latest=$(curl $dist/runner-latest$qualifier.txt)
 echo "Installing runner version $latest"
 curl $dist/runner-${latest}-linux-x64.zip >/tmp/runner.zip
 cd priv/runner
