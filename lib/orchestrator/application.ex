cdd875c51c00f4e1b771cd3d88a42d750f86df33
Nikko Campbell <1914937+nikkocampbell@users.noreply.github.com>
2021-10-12 11:55:36 -0400

    1459 handle 401 and 403 responses (#41)
    * slack reporter module
    
    * configure slack
    
    * Add error handler
    
    * slack channel env
    
    * messaging
    
    * env for slack api token
    
    * Only use slack reporter if configured
    
    * Update config check
    
    * update config

--- lib/orchestrator/application.ex
+++ lib/orchestrator/application.ex
@@ -21,6 +21,7 @@ defmodule Orchestrator.Application do
     config_fetch_fun = fn -> Orchestrator.APIClient.get_config(instance(), run_groups) end
 
     configure_api_token()
+    configure_slack_reporter()
 
     children = [
       CanaryIPA.Agent,
@@ -53,6 +54,8 @@ defmodule Orchestrator.Application do
 
   def api_token, do: Application.get_env(:orchestrator, :api_token)
 
+  def slack_api_token, do: Application.get_env(:slack, :api_token)
+
   def ipa_loopback_only?, do: Application.get_env(:orchestrator, :ipa_loopback_only)
 
   defp configure_configs(), do: Orchestrator.Configuration.init()
@@ -79,6 +82,7 @@ if Mix.env() == :test do
   # For now, the simplest way to make tests just do tests, not configure/start anything.
   defp filter_children(_children), do: []
   defp configure_api_token, do: :ok
+  defp configure_slack_reporter, do: :ok
 else
   defp filter_children(children), do: children
   defp configure_api_token do
@@ -92,6 +96,15 @@ else
       end
 
     Application.put_env(:orchestrator, :api_token, token)
+  end
+  defp configure_slack_reporter do
+    token = System.get_env("SLACK_API_TOKEN")
+    |> Orchestrator.Configuration.translate_value()
+    Application.put_env(:slack, :api_token, token)
+
+    channel = System.get_env("SLACK_ALERTING_CHANNEL")
+    |> Orchestrator.Configuration.translate_value()
+    Application.put_env(:slack, :reporting_channel, channel)
   end
 end
 
---
+++ lib/orchestrator/slack_reporter.ex
@@ -0,0 +1,53 @@
+defmodule Orchestrator.SlackReporter do
+  require Logger
+
+  @slack_url "https://slack.com/api/chat.postMessage"
+
+  def send_monitor_error(monitor_logical_name, check_logical_name, message) do
+    channel = Application.get_env(:slack, :reporting_channel)
+
+    if is_nil(channel) do
+      Logger.warn("Slack Reporter channel not configured. Unable to send message")
+      {:error, :channel_not_set}
+    else
+      body = %{
+        channel: channel,
+        text: ":x: *Error running #{monitor_logical_name}/#{check_logical_name} in #{Orchestrator.Application.instance()}*\n#{message}"
+      }
+
+      case Jason.encode(body) do
+        {:ok, json} ->
+          send_message(json)
+
+        {:error, _} ->
+          Logger.error("Failed to encode slack monitor error: #{message}")
+          {:error, :invalid_message}
+      end
+    end
+  end
+
+  def send_message(message) do
+    case Application.get_env(:slack, :api_token) do
+      nil ->
+        Logger.warn("Slack Reporter api token not configured. Unable to send message")
+        {:error, :api_token_not_set}
+
+      token ->
+        send_message(message, token)
+    end
+  end
+
+  def send_message(message, token) do
+    headers = [
+      {"Authorization", "Bearer #{token}"},
+      {"Content-Type", "application/json; charset=UTF-8"}
+    ]
+
+    HTTPoison.post(@slack_url, message, headers)
+  end
+
+  def is_configured?() do
+    !(is_nil(Application.get_env(:slack, :api_token)) ||
+      is_nil(Application.get_env(:slack, :reporting_channel)))
+  end
+end
