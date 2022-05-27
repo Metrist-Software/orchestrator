9991e2c3d0c2f7b609c5aecd306dbdababc7b40d
Daven Casia <77761194+fmterrorf@users.noreply.github.com>
2022-05-27 12:51:15 -0600

    Remove logging

--- config/config.exs
+++ config/config.exs
@@ -3,5 +3,3 @@ import Config
 config :ex_aws,
   access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, {:awscli, :system, 30}, :instance_role],
   secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, {:awscli, :system, 30}, :instance_role]
-
-  IO.inspect("HELLOOOOOO")
