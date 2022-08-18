1f64c20b92fb8cbd675d90722f9cc9afe5c7b263
Daven Casia <daven@metrist.io>
2022-08-18 10:25:21 -0600

    Set default to false

--- config/dev.exs
+++ config/dev.exs
@@ -2,7 +2,7 @@ import Config
 
 config :orchestrator,
   enable_host_telemetry?:
-    System.get_env("METRIST_ENABLE_HOST_TELEMETRY", "true") |> String.to_existing_atom(),
+    System.get_env("METRIST_ENABLE_HOST_TELEMETRY", "false") |> String.to_existing_atom(),
   api_token: System.get_env("CANARY_API_TOKEN"),
   slack_api_token: System.get_env("SLACK_API_TOKEN"),
   slack_reporting_channel: System.get_env("SLACK_ALERTING_CHANNEL")
