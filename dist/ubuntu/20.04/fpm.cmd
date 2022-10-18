5963a1bca82c5caca671671c7d0cc4c9ed1086b5
Daven Casia <daven@metrist.io>
2022-10-18 09:59:46 -0600

    Revert change

--- dist/ubuntu/20.04/fpm.cmd
+++ dist/ubuntu/20.04/fpm.cmd
@@ -1,1 +1,1 @@
--t rpm  -d procps -d libc6 -d libncurses5 -d libssl1.1 -d libgcc1 -d libstdc++6 -d libsctp1
+-t deb  -d procps -d libc6 -d libncurses5 -d libssl1.1 -d libgcc1 -d libstdc++6 -d libsctp1
