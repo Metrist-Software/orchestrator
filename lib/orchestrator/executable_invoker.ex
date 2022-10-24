3eedfd54196c9c80cae57f791551c0f9b3801bfe
Cees de Groot <cg@evrl.com>
2022-10-24 14:43:23 -0400

    Cleanup formatting

--- lib/orchestrator/executable_invoker.ex
+++ lib/orchestrator/executable_invoker.ex
@@ -16,24 +16,26 @@ defmodule Orchestrator.ExecutableInvoker do
     executable = unless executable, do: get_executable(config), else: executable
 
     Logger.debug("Running #{executable}")
+
     if not File.exists?(executable) do
       raise "Executable #{executable} does not exist, exiting!"
     end
 
     Orchestrator.Invoker.run_monitor(config, opts, fn ->
-        Port.open({:spawn_executable, executable}, [
-          :binary,
-          :stderr_to_stdout,
-          cd: Path.dirname(executable)
-         ])
+      Port.open({:spawn_executable, executable}, [
+        :binary,
+        :stderr_to_stdout,
+        cd: Path.dirname(executable)
+      ])
     end)
   end
 
   defp get_executable(config) do
-    {dir, executable} = {Orchestrator.Invoker.maybe_download(config.run_spec.name), config.run_spec.name}
+    {dir, executable} =
+      {Orchestrator.Invoker.maybe_download(config.run_spec.name), config.run_spec.name}
+
     # executable is relative to dir, make it absolute
     executable = Path.join(dir, executable)
     Path.expand(executable)
   end
-
 end
