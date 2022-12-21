8ad709d2f2efe772ebf94be49ff020c7f3c6bfe1
Daven Casia <daven@metrist.io>
2022-12-21 18:36:57 +0000

    Stop orchestrator if it's running

--- dist/amazon-linux/2/after-remove.sh
+++ dist/amazon-linux/2/after-remove.sh
@@ -4,8 +4,7 @@ set -ex
 
 if systemctl is-enabled --quiet metrist-orchestrator; then
   systemctl stop metrist-orchestrator
-  systemctl disable metrist-orchestrator
-  systemctl reset-failed metrist-orchestrator
-  systemctl daemon-reload
 fi
+
+systemctl daemon-reload
 
