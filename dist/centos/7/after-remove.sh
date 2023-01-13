16f990da4bb69615a919feacd8758c0ad8808b39
Nikko Campbell <nikko@metrist.io>
2023-01-13 17:01:06 -0500

    Fix after-remove script

--- dist/centos/7/after-remove.sh
+++ dist/centos/7/after-remove.sh
@@ -3,9 +3,5 @@ set -e
 
 SERVICE="metrist-orchestrator.service"
 
-if systemctl is-enabled --quiet $SERVICE; then
-  systemctl stop $SERVICE
-  systemctl disable $SERVICE
-  systemctl reset-failed $SERVICE
-  systemctl daemon-reload
-fi
+systemctl daemon-reload
+systemctl stop $SERVICE
