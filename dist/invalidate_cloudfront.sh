fcdb15305389d5e9c5ea5e5015b06116a8e14ab3
Dave Gray <dave@davegray.ca>
2023-02-07 11:04:18 -0700

    We have no $dist var here

--- dist/invalidate_cloudfront.sh
+++ dist/invalidate_cloudfront.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 
 echo "Invalidating cloudfront Orchestrator distributions"
 
-aws cloudfront create-invalidation --distribution-id E1FRDOED06X2I8 --paths "/orchestrator/$dist/*" --no-cli-pager
+aws cloudfront create-invalidation --distribution-id E1FRDOED06X2I8 --paths "/orchestrator/*" --no-cli-pager
